0952603de7d0b7e3c5e287f5f053542b9f647030
Carolina Pineda <gpinedacarolina@gmail.com>
2023-05-30 22:08:02 -0600

    add

--- controller/usuarios.js
+++ controller/usuarios.js
@@ -16,22 +16,18 @@ const usuariosGet = async(req, res = response) =>{
     const {limite = 5, desde=0} = req.query;
     const query = {estado: true};
 
-    const usuarios = await Usuario.find(query)
-        .skip(Number(desde))
-        .limit(Number(limite));
-
-const total = await Usuario.countDocuments(query);
-
-const resp = Promise.all([
+const resp = await Promise.all([
     Usuario.count(query),
     Usuario.find(query)
-    .skip(Number(desde))
-    .limit(Number(limite))
+        .skip(Number(desde))
+        .limit(Number(limite))
 ])
 
     res.json({
-        total,
-        usuarios
+        res
+        // total,
+        // usuarios
+
     })
 };
 
