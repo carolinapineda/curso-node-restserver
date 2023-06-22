7510f43641705093275e0a97d20debe6e28e88aa
Carolina Pineda Grifaldo <gpinedacarolina@gmail.com>
2023-06-22 14:46:03 -0600

    add

--- routes/cargas.js
+++ routes/cargas.js
@@ -3,7 +3,7 @@ const { check } = require('express-validator');
 
 
 const { validarCampos } = require('../middlewares/validar-campos');
-const { cargarArchivos, actualizarImagen } = require('../controller/cargas');
+const { cargarArchivos, actualizarImagen, mostrarImagen, actualizarImagenCloudinary } = require('../controller/cargas');
 const { coleccionesPermitidas } = require('../helpers');
 const { validarArchivosSubir } = require('../middlewares');
 
@@ -17,6 +17,13 @@ router.put('/:coleccion/:id', [
     check('id', 'El id debe de ser de mongo').isMongoId(),
     check('coleccion').custom( c => coleccionesPermitidas( c, ['usuarios', 'productos'])),
     validarCampos
-], actualizarImagen)
+], actualizarImagenCloudinary)
+// actualizarImagen)
+
+router.get('/:coleccion/:id', [
+    check('id','El id debe de ser de mongo').isMongoId(),
+    check('coleccion').custom( c => coleccionesPermitidas( c, ['usuarios','productos'] ) ),
+    validarCampos
+], mostrarImagen )
 
 module.exports = router;
