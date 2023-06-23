3f7eba678295b14f53f835115a007fba00d0b091
Carolina Pineda Grifaldo <gpinedacarolina@gmail.com>
2023-06-23 11:00:16 -0600

    add

--- middlewares/index.js
+++ middlewares/index.js
@@ -4,10 +4,12 @@ const validaCampos = require('../middlewares/validar-campos');
 const validarJWT = require('../middlewares/validar-jwt');
 const validaRoles = require('../middlewares/validar-roles');
 const validarArchivo = require('../middlewares/validar-archivo');
+const validarImagen = require('../middlewares/validar-imagen');
 
 module.exports = {
     ...validaCampos,
     ...validarJWT,
     ...validaRoles,
-    ...validarArchivo
+    ...validarArchivo,
+    ...validarImagen
 }
